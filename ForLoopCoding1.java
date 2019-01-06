45cd8e3cb9001d04bc50eb0da5d09a3e94a8316c
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-01-06 23:25:45 +0530

    code changes and pushing to git hub

---
+++ ForLoopCoding1.java
@@ -0,0 +1,14 @@
+package com.java.conditionscoding;
+
+public class ForLoopCoding1 {
+
+	public static void main(String[] args) {
+int i=0;
+for (i=0;i<6;i++)
+{
+	System.out.println(i);
+}
+
+	}
+
+}
---
+++ ForLoopCoding4.java
@@ -0,0 +1,15 @@
+package com.java.conditionscoding;
+import java.util.*;
+public class ForLoopCoding4 {
+
+	
+	public static void main(String[] args)
+	{
+		 ArrayList<String> a[]= {"raka","ram"};
+		 for (int i=0;i<2;i++)
+		 {
+			 System.out.println(a[i]);
+		 }
+		
+	}
+}
